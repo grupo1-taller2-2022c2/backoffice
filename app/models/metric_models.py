c3abd16854106d75c96d10a3e7b573af21827274
Agustin Gabrielli <agusgabrielli9@gmail.com>
2022-10-29 17:17:24 -0300

    refactor: improved documentation

--- app/models/metric_models.py
+++ app/models/metric_models.py
@@ -1,14 +1,6 @@
 from sqlalchemy import Column, Integer, Date, String
 from sqlalchemy.sql import func
 from app.models import Base
-from abc import ABC, abstractmethod
-
-
-class Counter(ABC):
-
-    @abstractmethod
-    def count():
-        pass
 
 
 class Registration(Base):
@@ -16,15 +8,3 @@ class Registration(Base):
     reg_id = Column(Integer, autoincrement=True, primary_key=True)
     method = Column(String(50), nullable=False)
     date_registered = Column(Date, server_default=func.current_date())
-
-
-class FederatedIdRegistrationCounter(Counter):
-    def count(session, date_from):
-        print(date_from)
-        return session.query(Registration).filter(Registration.method.like('federatedidentity'), Registration.date_registered >= date_from).count()
-
-
-class MailPasswordRegistrationCounter(Counter):
-    def count(session, date_from):
-        print(date_from)
-        return session.query(Registration).filter(Registration.method.like('mailpassword'), Registration.date_registered >= date_from).count()
--- app/routes/metrics_routes.py
+++ app/routes/metrics_routes.py
@@ -1,6 +1,6 @@
 import datetime
 import os
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Query
 from starlette import status
 from app.database import SessionLocal, get_db
 from app.cruds import metrics_cruds
@@ -8,26 +8,30 @@ from app.cruds import metrics_cruds
 router = APIRouter()
 url_base = os.getenv('USERS_BASE_URL')
 
-# Endpoints for Users microservice (to update counters)
 
-
-@router.post("/registrations", status_code=status.HTTP_200_OK)
-def add_one_to_registration_count(method: str = "mailpassword",  db: SessionLocal = Depends(get_db)):
+def check_correct_method(method):
     if method not in ["mailpassword", "federatedidentity"]:
         raise HTTPException(
             status_code=409, detail="method must be mailpassword or federatedidentity")
+
+
+# ------------Endpoints for Users microservice (to update counters))-----------------
+
+
+@router.post("/registrations", status_code=status.HTTP_200_OK)
+def add_one_to_registration_count(method: str = Query(default="mailpassword", description="Should be mailpassword or federatedidentity"),
+                                  db: SessionLocal = Depends(get_db)):
+    check_correct_method(method)
 
     return metrics_cruds.add_registration(method, db)
 
 
-# Endpoints for frontend (to display metrics)
+# ------------Endpoints for frontend (to display metrics)-----------------
 
 # e.g. /registrations?method=mailpassword
-# response_model=List[metric_schemas.MetricSchema],
 @router.get("/registrations", status_code=status.HTTP_200_OK)
-def get_registrations_count(method: str = "mailpassword", from_date: datetime.date = datetime.date.today(),  db: SessionLocal = Depends(get_db)):
-    if method not in ["mailpassword", "federatedidentity"]:
-        raise HTTPException(
-            status_code=409, detail="method must be mailpassword or federatedidentity")
+def get_registrations_count(method: str = Query(default="mailpassword", description="Should be mailpassword or federatedidentity"),
+                            from_date: datetime.date = datetime.date.today(),  db: SessionLocal = Depends(get_db)):
+    check_correct_method(method)
 
     return metrics_cruds.count_registrations(method, from_date, db)
