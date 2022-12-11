e7a2fdfb83ee28a781a42b5f49ddaa362c1d2d3e
agus815 <asu@fi.uba.ar>
2022-12-11 01:36:47 -0300

    feat: add full user info

--- app/routes/users_admin_routes.py
+++ app/routes/users_admin_routes.py
@@ -12,7 +12,7 @@ url_base = os.getenv('USERS_BASE_URL')
 wallets_url_base = os.getenv('WALLETS_BASE_URL')
 
 
-@router.get("/", response_model=List[user_schemas.UserSchema], status_code=status.HTTP_200_OK)
+@router.get("/", response_model=List[user_schemas.UserFullInfo], status_code=status.HTTP_200_OK)
 def get_users():
     url = url_base + "/users/"
     response = requests.get(url=url)
